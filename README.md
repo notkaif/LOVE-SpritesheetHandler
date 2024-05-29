f1ec37c813c3e42dddf2175372a1bd38ab7449ec
Not_Kaif <notkaif.p@gmail.com>
2024-05-29 21:59:25 +0100

    LOVE Spritesheet Handler v2
    added stuff

--- README.md
+++ README.md
@@ -1,31 +1,90 @@
-# LOVE-SpritesheetHandler
-Sparrow v2 and Starling spritesheet handler for LÖVE 2D Game Engine.
+# LOVE-SpritesheetHandler v2
+
+Sparrow v2 and (partially) Starling spritesheet handler for LÖVE 2D Game Engine.
 This can handle multiple animations from one sprite sheet.
 
-Pop the spritehandler.lua file into your project.
+## What the heck is Sparrow v2 or Starling?
 
-Heres a few snippets for use in the main.lua fie.
+Sparrow v2 is a spritesheet format that uses a image file and an XML file to store sprite data.
+It is used in the game Friday Night Funkin'
+
+A small example of a spritesheet's XML data:
+
+```xml
+<TextureAtlas imagePath="BOYFRIEND.png">
+<SubTexture name="BF idle dance0000" x="0" y="2344" width="406" height="392" frameX="-1" frameY="-20" frameWidth="411" frameHeight="412"/>
+<SubTexture name="BF idle dance0001" x="0" y="2344" width="406" height="392" frameX="-1" frameY="-20" frameWidth="411" frameHeight="412"/>
+<SubTexture name="BF idle dance0002" x="416" y="2344" width="408" height="393" frameX="0" frameY="-19" frameWidth="411" frameHeight="412"/>
+<SubTexture name="BF idle dance0003" x="416" y="2344" width="408" height="393" frameX="0" frameY="-19" frameWidth="411" frameHeight="412"/>
+<SubTexture name="BF idle dance0004" x="834" y="2344" width="405" height="398" frameX="-3" frameY="-14" frameWidth="411" frameHeight="412"/>
+<SubTexture name="BF idle dance0005" x="834" y="2344" width="405" height="398" frameX="-3" frameY="-14" frameWidth="411" frameHeight="412"/>
+<SubTexture name="BF idle dance0006" x="1249" y="2344" width="410" height="411" frameX="-1" frameY="-1" frameWidth="411" frameHeight="412"/>
+<SubTexture name="BF idle dance0007" x="1249" y="2344" width="410" height="411" frameX="-1" frameY="-1" frameWidth="411" frameHeight="412"/>
+<SubTexture name="BF idle dance0008" x="1669" y="2344" width="408" height="412" frameX="-2" frameY="0" frameWidth="411" frameHeight="412"/>
+</TextureAtlas>
+```
+
+## How the heck do I use this?
+
+Put the spritehandler.lua file somewhere in your project.
+
+Make sure to require the spritesheet handler in your code
 
 ```lua
 local SpriteHandler = require("spritehandler") -- Change to the directory of the spritehandler lua file.
 ```
-This adds the following functions below. Make sure to have the spritehandler.lua file somewhere in your project and is in the directory put above.
+
+Now you can start using it. Here is a simple example of how you can use the spritehandler:
 
 ```lua
-function love.load()
-    _G.sprite1 = SpriteHandler.new()
-    sprite1:setImage(love.graphics.newImage("spritesheet"))
-    sprite1:setXML(love.filesystem.read("spritesheetxml"))
-    sprite1:setFPS(24)
-    sprite1:setSizeMultiplier(2)
-    sprite1:Play("animation")
 
-    -- You can also Pause, Resume and Stop the animation.
-    sprite1:Stop()
-    sprite1:Pause()
-    sprite!:Resume()
-    
+local sprite
+
+function love.load()
+    sprite = spritehandler.new()
+    sprite1:setImage("BOYFRIEND.png") -- Path to the image
+    sprite1:setXML("BOYFRIEND.xml") -- Path to the XML file
+    sprite1:setFPS(24) -- Sets the FPS
+
+    sprite1:play("BF idle dance") -- Plays the animation provided
 end
+
+function love.update(dt)
+    sprite:update(dt) -- Updates the sprite
+end
+
+function love.draw()
+    sprite1:draw(x, y, rotation) -- Draws the sprite with the x, y and rotation provided
+end
+
+```
+
+## Theres more functions!?
+
+Theres a few more, and you can always add your own.
+
+```lua
+
+sprite:stop() -- Stops the current animation (sprite will become blank)
+sprite:pause() -- Simply pauses the animation
+sprite:resume() -- Simply resumes the animation
+
+sprite:mouseTouching() -- Returns a boolean if the mouse is touching the sprite or not.
+
+```
+
+## One more thing
+
+You can store extra data about a sprite simply by creating a variable in the sprite.
+
+Example:
+
+```lua
+
+sprite.numberExample = 100
+sprite.stringExample = "hello"
+sprite.boolExample = true
+
 ```
 
 Now I need to try implementing Adobe Animate's texture atlases....
--- spritehandler.lua
+++ spritehandler.lua
@@ -1,95 +1,127 @@
 local SpriteHandler = {}
 
 function SpriteHandler.new()
-    local self = {}
+    local self = {
+        image = nil,
+        animations = {},
+        currentAnimation = nil,
+        currentFrame = 1,
+        paused = false,
+        fps = 24,
+        checked = false,
+        isMouseTouching = false,
+        width = 0,
+        height = 0
+    }
 
-    local image
-    local animations = {}
-    local currentAnimation = nil
-    local currentFrame = 1
-    local fps = 12
-    local sizeMultiplier = 1
-
-    function self:setImage(img)
-        image = img
+    -- Set the image for the sprite handler
+    function self:setImage(imgPath)
+        self.image = love.graphics.newImage(imgPath)
     end
 
-    function parseXML(data)
-        local animations = {}
-        local _, _, imagePath = data:find('imagePath="([^"]+)"')
-
-        for name, x, y, width, height in data:gmatch('<SubTexture name="([^"]+)" x="(%d+)" y="(%d+)" width="(%d+)" height="(%d+)"') do
-            local animationName = name:match("([^%d]+)") -- Extracting animation name without frame number
-            if not animations[animationName] then
-                animations[animationName] = {}
-            end
-            table.insert(animations[animationName], {
-                quad = love.graphics.newQuad(tonumber(x), tonumber(y), tonumber(width), tonumber(height), image:getWidth(), image:getHeight()),
-                width = tonumber(width),
-                height = tonumber(height)
-            })
+    -- Parse XML and set animations STARLING AND SPARROW V2 LIMITED FUNCTIONALITY
+    function self:setXML(filePath)
+        local data = love.filesystem.read(filePath)
+        if not data then
+            print("Error: Could not read file " .. filePath)
+            return
         end
 
-        return animations, imagePath
+        local imageWidth = self.image:getWidth()
+        local imageHeight = self.image:getHeight()
+
+        for name, x, y, width, height, frameX, frameY, frameWidth, frameHeight in data:gmatch('<SubTexture name="([^"]+)" x="(%d+)" y="(%d+)" width="(%d+)" height="(%d+)" frameX="([%-]?%d+)" frameY="([%-]?%d+)" frameWidth="(%d+)" frameHeight="(%d+)"') do
+            local animationName = name:match("([^%d]+)")
+            if not self.animations[animationName] then
+                self.animations[animationName] = {}
+            end
+            table.insert(self.animations[animationName], {
+                quad = love.graphics.newQuad(
+                    tonumber(x), tonumber(y),
+                    tonumber(width), tonumber(height),
+                    imageWidth, imageHeight
+                ),
+                width = tonumber(frameWidth),
+                height = tonumber(frameHeight),
+                offsetX = tonumber(frameX),
+                offsetY = tonumber(frameY)
+            })
+        end
     end
 
-    function self:setXML(data)
-        animations, imagePath = parseXML(data)
+    -- Set frames per second
+    function self:setFPS(fps)
+        self.fps = fps
     end
 
-    function self:setFPS(f)
-        fps = f
-    end
-
-    function self:setSizeMultiplier(s)
-        sizeMultiplier = s
-    end
-
-    function self:Play(animationName)
-        if animations[animationName] then
-            currentAnimation = animationName
-            currentFrame = 1
+    -- Play a specified animation
+    function self:play(animationName)
+        if self.animations[animationName] then
+            self.currentAnimation = animationName
+            self.currentFrame = 1
         else
             print("Animation '" .. animationName .. "' not found.")
         end
     end
 
-    function self:Stop()
-        currentAnimation = nil
+    -- Stop the current animation
+    function self:stop()
+        self.currentAnimation = nil
     end
 
-    function self:Pause()
-        currentAnimation = nil
+    -- Pause the current animation
+    function self:pause()
+        self.paused = true
     end
 
-    function self:Resume()
-        if currentAnimation == nil and animations[currentAnimation] then
-            currentAnimation = currentAnimation
+    -- Resume the current animation
+    function self:resume()
+        self.paused = false
+    end
+
+    -- Check if the mouse is touching the sprite
+    function self:mouseTouching()
+        if self.checked then
+            self.checked = false
+            return self.isMouseTouching
         end
     end
 
+    -- Update the sprite handler
     function self:update(dt)
-        if currentAnimation then
-            currentFrame = currentFrame + fps * dt
-            if currentFrame >= #animations[currentAnimation] + 1 then
-                currentFrame = 1
+        if self.currentAnimation and not self.paused then
+            self.currentFrame = self.currentFrame + self.fps * dt
+            if self.currentFrame >= #self.animations[self.currentAnimation] + 1 then
+                self.currentFrame = 1
             end
         end
     end
 
-    function self:draw(x, y)
-        if currentAnimation then
-            local animationFrames = animations[currentAnimation]
-            local frame = math.floor(currentFrame)
-            if frame > #animationFrames then
-                frame = #animationFrames
-            end
+    -- Draw the sprite at a given position
+    function self:draw(x, y, rotation)
+        if self.currentAnimation then
+            local animationFrames = self.animations[self.currentAnimation]
+            local frame = math.min(math.floor(self.currentFrame), #animationFrames)
             local animation = animationFrames[frame]
             local quad = animation.quad
-            local width = animation.width * sizeMultiplier
-            local height = animation.height * sizeMultiplier
-            love.graphics.draw(image, quad, x, y, 0, sizeMultiplier, sizeMultiplier)
+            local width = animation.width
+            local height = animation.height
+            local offsetX = animation.offsetX
+            local offsetY = animation.offsetY
+
+            local mouseX, mouseY = love.mouse.getPosition()
+
+            self.checked = true
+            self.isMouseTouching = mouseX >= x and mouseX <= x + width and mouseY >= y and mouseY <= y + height
+
+            love.graphics.draw(self.image, quad, x - offsetX, y - offsetY, rotation, 1, 1, width/2, height/2)
         end
+    end
+
+    -- Deletes the sprite
+    function self:destroy()
+        self.image = nil
+        self.animations = {}
     end
 
     return self
